a7e18281b19ff27888737f2a1a10bb44d6e14572
mofiazdevops <mofiaz4000@gmail.com>
2025-03-04 15:09:49 +0500

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -1,4 +1,8 @@
 locals {
   resource_group_name = "Tayyba"
   location = "East Asia"
+  virtual_network={                              
+    name = "al-nafi-net"
+    address_space = "10.0.0.0/16"
+  }
 }
